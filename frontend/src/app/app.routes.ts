213dffb26135052fca30e661bd582cb032898052
GadielB <gadi77altair.gabg@gmail.com>
2025-02-09 13:11:48 -0400

    agregando rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 
+//realizando rutas para la navegacion entre componentes
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
